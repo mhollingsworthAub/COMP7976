5867309dc8b5d4ca568808c8043baf295d20ba9f
Michael Hollingsworth <hollimi@auburn.edu>
2017-09-10 08:39:18 -0500

    first

---
+++ test.py
@@ -0,0 +1,6 @@
+import urllib.request
+siteHtml = urllib.request.urlopen("http://www.auburn.edu").read()
+output = open("output.txt", "w")
+output.write(siteHtml.decode("utf-8"))
+output.flush()
+output.close()
