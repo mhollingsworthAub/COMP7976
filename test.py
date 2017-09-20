29d79ce234ad17bb698ca59d4d54debd52c6d37d
Michael Hollingsworth <hollimi@auburn.edu>
2017-09-20 11:01:32 -0500

    fixed rel links

--- test.py
+++ test.py
@@ -1,44 +1,77 @@
 import urllib.request
 from bs4 import BeautifulSoup as bs
 import os.path
+import time
+from multiprocessing.dummy import Pool as ThreadPool, Process, Queue
+pool = ThreadPool(8) 
+
 
 htmlSource = os.getcwd() + "/htmlSource/" #directory to store all the html source text files
-maxDepth = 2 
+maxDepth = 3
+now = time.time()
 
-def findChildren(link, depth): #This will find all child URLS (in 'a' elements) from link
-    depth += 1
+class Find:
 
-    if depth > maxDepth:
-        return False
-    try:
-        fileName = htmlSource + link.replace("/", "_").replace(":", "_")
-        if os.path.exists(fileName + ".txt"):
-            print("found file " + fileName + ".txt")
-            return True
+    def findChildren(self, link, depth): #This will find all child URLS (in 'a' elements) from link
+        depth += 1
 
-        print("Processing " + link)
-        siteHtml = urllib.request.urlopen(link).read()        
-        
-        output = open(fileName + ".txt", "w")
-        output.write(siteHtml.decode("utf-8"))
-        output.flush()
-        output.close()
+        if depth > maxDepth:
+            return False
+        try:
+            fileName = htmlSource + link.replace("/", "_").replace(":", "_")
+            if os.path.exists(fileName + ".txt"):
+                print("found file " + fileName + ".txt")
+                return True
 
-        soup = bs(siteHtml.decode("utf-8"), "html.parser")
+            print("Processing " + link)
+            siteHtml = urllib.request.urlopen(link).read()        
+            
+            output = open(fileName + ".txt", "w")
+            output.write(siteHtml.decode("utf-8"))
+            output.flush()
+            output.close()
 
+            rawHTML = bs(siteHtml.decode("utf-8"), "html.parser")
 
-        for link in soup.find_all("a"):
-            if(link.get("href") is not None):
-                if "http://" in link.get("href"): 
-                    findChildren(link.get("href"), depth)
-    except:
-        return False       
+            
+            for childLink in rawHTML.find_all("a"):
+                if(childLink.get("href") is not None):
+                    
+                    #print("**" + childLink.get("href")[0:2] + "**\n")
+                    if "http://" in childLink.get("href"): 
+                        self.findChildren(childLink.get("href"), depth)
+                    elif childLink.get("href")[0:2] == "//":
+                        var = childLink.get("href")
+                        self.findChildren("http:" + childLink.get("href"), depth)
+                    elif not "https://" in childLink.get("href"):
+                        l = childLink.get("href")
+                        if l[0] == '/':
+                            l = l[1:]
+                        self.findChildren(link[:link.rfind('/') + 1] + l, depth)
+        except:
+            return False       
+ 
 
-def makeGram(directory):
-    for fileName in os.listdir(directory):
-        unigram = open(directory + fileName.replace(".txt", ".count"), 'w')
+    def makeGram(self, directory):
+        for fileName in os.listdir(directory):
+            unigram = open(directory + fileName.replace(".txt", ".vec"), 'w')
+            counter = [0] * 95
+            fileContents = open(directory + fileName, "r").read()
+            for char in fileContents:
+                if ord(char) > 31 and ord(char) < 127:  
+                    counter[ord(char) - 32 ] += 1
+
+            s = str(counter[0])
+            for i in range (1, 94):
+                s = s + "," + str(counter[i])
+            unigram.write(s)
+            unigram.flush()
+            unigram.close()
+
+    def makeVec(self, fileName):
+        unigram = open(htmlSource + fileName.replace(".txt", ".vec"), 'w')
         counter = [0] * 95
-        fileContents = open(directory + fileName, "r").read()
+        fileContents = open(htmlSource + fileName, "r").read()
         for char in fileContents:
             if ord(char) > 31 and ord(char) < 127:  
                 counter[ord(char) - 32 ] += 1
@@ -50,8 +83,17 @@ def makeGram(directory):
         unigram.flush()
         unigram.close()
 
+
 if os.path.exists(htmlSource) is False:
     os.makedirs(htmlSource)
+f = Find()
+f.findChildren("http://about.ask.com/", 0)
 
-findChildren("http://www.auburn.edu", 0)
-makeGram(htmlSource)
+#f.findChildren(ld)
+#f.makeGram(htmlSource)
+print("Starting vec")
+pool.map(f.makeVec, os.listdir(htmlSource))
+pool.close()
+pool.join()
+
+print(time.time() - now)
